546bd8198c7d5d6f8b54ba985661c40176a78b79
jaspershen <shenxt1990@163.com>
2022-03-08 00:21:38 -0800

    fix small bug

--- R/dplyr_rename.R
+++ R/dplyr_rename.R
@@ -3,37 +3,38 @@
 #' @importFrom rlang quos !!!
 #' @importFrom dplyr rename
 #' @export
-rename.mass_dataset <- function(.data, ...) {
-  dots <- rlang::quos(...)
-  
-  if (length(.data@activated) == 0) {
-    stop("activate you object using activate_mass_dataset first.\n")
+rename.mass_dataset <-
+  function(.data, ...) {
+    dots <- rlang::quos(...)
+    
+    if (length(.data@activated) == 0) {
+      stop("activate you object using activate_mass_dataset first.\n")
+    }
+    
+    x <-
+      slot(object = .data, name = .data@activated)
+    
+    x <-
+      rename(x, !!!dots)
+    
+    slot(object = .data, name = .data@activated) = x
+    
+    if (.data@activated == "expression_data") {
+      .data@sample_info$sample_id = colnames(x)
+    }
+    
+    if (.data@activated == "sample_info") {
+      .data@sample_info_note$name <-
+        colnames(.data@sample_info)
+    }
+    
+    if (.data@activated == "variable_info") {
+      .data@variable_info_note$name$name <-
+        colnames(.data@variable_info)
+    }
+    
+    return(.data)
   }
-  
-  x <-
-    slot(object = .data, name = .data@activated)
-  
-  x <-
-    rename(x, !!!dots)
-  
-  slot(object = .data, name = .data@activated) = x
-  
-  if (.data@activated == "expression_data") {
-    .data@sample_info$sample_id = colnames(x)
-  }
-  
-  if (.data@activated == "sample_info") {
-    .data@sample_info_note$name <- 
-      colnames(.data@sample_info)
-  }
-  
-  if (.data@activated == "variable_info") {
-    .data@variable_info$name <- 
-      colnames(.data@variable_info)
-  }
-  
-  return(.data)
-}
 
 #' @importFrom dplyr rename
 #' @export
