2f4fe2f4391c01321ceec290e0722109f89fd500
jaspershen <shenxt1990@163.com>
2022-05-06 16:52:54 -0700

    fix bugs

--- R/dplyr_left_join.R
+++ R/dplyr_left_join.R
@@ -9,6 +9,44 @@ left_join.mass_dataset <-
            suffix = c(".x", ".y"),
            ...,
            keep = FALSE) {
+    left_join_mass_dataset(
+      x = x,
+      y = y,
+      by = by,
+      copy = copy,
+      suffix = suffix,
+      keep = keep,
+      ...
+    )
+  }
+
+#' @importFrom dplyr left_join
+#' @export
+dplyr::left_join
+
+#' @title Left join for mass_dataset
+#' @description Left join for mass_dataset
+#' @author Xiaotao Shen
+#' \email{shenxt1990@@outlook.com}
+#' @param x (required) mass_dataset class object.
+#' @param y A data frame.
+#' @param by by, see ?_left_join
+#' @param copy copy, see ?_left_join
+#' @param suffix suffix, see ?_left_join
+#' @param keep keep, see ?_left_join
+#' @param ... Other arguments, see ?_left_join
+#' @return a mass_dataset class object.
+#' @export 
+#' @importFrom dplyr left_join
+#' @export
+left_join_mass_dataset <-
+  function(x,
+           y,
+           by = NULL,
+           copy = FALSE,
+           suffix = c(".x", ".y"),
+           ...,
+           keep = FALSE) {
     if (length(x@activated) == 0) {
       stop("activate you object using activate_mass_dataset first.\n")
     }
@@ -31,15 +69,15 @@ left_join.mass_dataset <-
     slot(object = x, name = x@activated) <- new_x
     
     ###sample_info
-    if(x@activated == "sample_info"){
-      if(!"sample_id" %in% colnames(new_x)){
+    if (x@activated == "sample_info") {
+      if (!"sample_id" %in% colnames(new_x)) {
         stop("You can't remove sample_id.\n")
       }
     }
     
     ###variable_info
-    if(x@activated == "variable_info"){
-      if(!"variable_id" %in% colnames(new_x)){
+    if (x@activated == "variable_info") {
+      if (!"variable_id" %in% colnames(new_x)) {
         stop("You can't remove variable_id\n")
       }
     }
@@ -49,6 +87,5 @@ left_join.mass_dataset <-
     return(x)
   }
 
-#' @importFrom dplyr left_join
-#' @export
-dplyr::left_join
+
+
